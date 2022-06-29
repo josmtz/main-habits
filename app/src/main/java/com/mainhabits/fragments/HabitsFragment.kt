da0bd6c5c343b5c58ffe330ff4132bbdcd8bcd65
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2022-06-29 10:43:18 -0400

    Add FAB on HabitsFragment

--- app/src/main/java/com/mainhabits/fragments/HabitsFragment.kt
+++ app/src/main/java/com/mainhabits/fragments/HabitsFragment.kt
@@ -5,16 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mainhabits.R
+import com.google.android.material.snackbar.Snackbar
+import com.mainhabits.databinding.FragmentHabitsBinding
 
 
 class HabitsFragment : Fragment() {
+
+    private var _binding: FragmentHabitsBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_habits, container, false)
+    ): View {
+        _binding = FragmentHabitsBinding.inflate(inflater, container, false)
+
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Create a habit", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
+        return binding.root
+
     }
 }
