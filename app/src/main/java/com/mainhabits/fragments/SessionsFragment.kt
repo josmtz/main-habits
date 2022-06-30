bf89bf7e259fcf1c6cf77ca6505be0dd62c81bee
Josue Martinez <46319076+josue-martinez@users.noreply.github.com>
2022-06-30 12:02:30 -0400

    Add Fab to SessionsFragment

--- app/src/main/java/com/mainhabits/fragments/SessionsFragment.kt
+++ app/src/main/java/com/mainhabits/fragments/SessionsFragment.kt
@@ -1,20 +1,34 @@
 package com.mainhabits.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.mainhabits.R
+import androidx.fragment.app.Fragment
+import com.google.android.material.snackbar.Snackbar
+import com.mainhabits.databinding.FragmentSessionsBinding
 
 
 class SessionsFragment : Fragment() {
+
+    private var _binding: FragmentSessionsBinding? = null
+    // This property is only valid between onCreateView and onDestroyView.
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_sessions, container, false)
+        _binding = FragmentSessionsBinding.inflate(inflater, container, false)
+
+
+        binding.sessionsFab.setOnClickListener { view ->
+            Snackbar.make(view, "Create a session", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+
+        return binding.root
     }
 }
