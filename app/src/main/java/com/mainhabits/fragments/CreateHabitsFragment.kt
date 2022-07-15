7bc942f8004e7c4185d1a2a12cdd52af4f23a913
Josue Martinez <46319076+josmtz@users.noreply.github.com>
2022-07-15 12:14:49 -0400

    Add CreateHabitsFragment

---
+++ app/src/main/java/com/mainhabits/fragments/CreateHabitsFragment.kt
@@ -0,0 +1,27 @@
+package com.mainhabits.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.mainhabits.R
+
+
+class CreateHabitsFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_create_habits, container, false)
+
+
+
+
+    }
+
+
+}
