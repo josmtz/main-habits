75ab61117fc042198dc44febfde40f18c6201b98
Josue Martinez <46319076+josmtz@users.noreply.github.com>
2022-07-15 12:16:37 -0400

    Update HabitsFragment.kt
    Add action to OnClickListener FAB

--- app/src/main/java/com/mainhabits/fragments/HabitsFragment.kt
+++ app/src/main/java/com/mainhabits/fragments/HabitsFragment.kt
@@ -1,11 +1,17 @@
 package com.mainhabits.fragments
 
+import android.R
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment.Companion.findNavController
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.snackbar.Snackbar
+import com.mainhabits.databinding.FragmentCreateHabitsBinding
 import com.mainhabits.databinding.FragmentHabitsBinding
 
 
@@ -24,6 +30,13 @@ class HabitsFragment : Fragment() {
 
 
         binding.habitsFab.setOnClickListener { view ->
+
+            val transaction = activity?.supportFragmentManager?.beginTransaction()
+            transaction?.replace(com.mainhabits.R.id.library_container, CreateHabitsFragment())
+            transaction?.disallowAddToBackStack()
+            transaction?.commit()
+
+
             Snackbar.make(view, "Create a habit", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
